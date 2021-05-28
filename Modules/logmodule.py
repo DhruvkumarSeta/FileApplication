c1ea48843b821d1c4d3d57b56339ce3a2fff180b
Dhruvkumar Seta <dhruvkumar.seta@hp.com>
2021-05-29 00:41:22 +0530

    Logging issue resolved

--- Modules/logmodule.py
+++ Modules/logmodule.py
@@ -5,8 +5,9 @@ from Constant import ZipperApp as zac
 def getlogger():
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
-    file_handler = logging.FileHandler(zac.logpath)
-    formatter = logging.Formatter(zac.logfomrat)
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
+    if not logger.hasHandlers():
+        file_handler = logging.FileHandler(zac.logpath)
+        formatter = logging.Formatter(zac.logfomrat)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
     return logger
